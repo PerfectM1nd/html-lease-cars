34aea368d4db25e458944c2423c593b98110720f
PerfectM1nd <gzagayko@gmail.com>
2023-11-26 08:48:08 +0300

    completed base layout, added new cars and contacts pages

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach((elem) => {
+  elem.addEventListener("click", open);
+})
+
+function open(event) {
+  const tabTarget = event.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach((elem) => {
+    elem.classList.remove('tabs__btn-item--active');
+  })
+
+  tabTarget.classList.add('tabs__btn-item--active');
+
+  tabContent.forEach((elem) => {
+    elem.classList.remove('tabs__content-item--active');
+  })
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
 const swiper = new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    effect: 'fade',
-    loop: true
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  effect: 'slide',
+  loop: true
 });
